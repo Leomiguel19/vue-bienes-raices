61efcc32015b3644e9b3677384b4cff0420f8854
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-06 23:31:40 -0400

    Limpiar proyecto e instalar vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-center" >Vue Bienes Raices</h1>
 </template>
