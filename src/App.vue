abdf025f5442c49f8060c9472441aaf2cd1d112c
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-06 23:42:16 -0400

    Primeros pasos con vuetify

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,21 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <v-card elevation="3" max-width="1200" class="mx-auto">
+    <v-layout>
+      <v-app-bar color="blue-darken-1">
+        <template v-slot:append>
+          <h1>Hola</h1>
+        </template>
+        <template v-slot:prepend>
+          <h1>Hola 2</h1>
+        </template>
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,5 @@
 </script>
 
 <template>
-  <h1 class="text-center" >Vue Bienes Raices</h1>
+  <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raices</h1>
 </template>
