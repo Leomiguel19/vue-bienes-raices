a31f61b2ac43980fd617923907fc3ebe579ab201
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-11 21:49:16 -0400

    Subir imágenes a Firebase

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -4,8 +4,11 @@
     import { useFirestore } from 'vuefire'
     import { useRouter } from 'vue-router'
     import { validationSchema, imageSchema } from '@/validation/propiedadSchema'
+    import useImage from '@/composables/useImages'
 
     const items = [1, 2, 3, 4, 5]
+
+    const { uploadImage } = useImage()
 
     const router = useRouter()
     const db = useFirestore()
@@ -67,6 +70,7 @@
                 class="mb-5" 
                 v-model="imagen.value.value"
                 :error-messages="imagen.errorMessage.value"
+                @change="uploadImage"
             />
 
             <v-text-field 
